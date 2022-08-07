f2b3a37667570ec32ded9246c86341185f5a1462
Kiran Anand <liteninkiran@hotmail.com>
2022-08-07 19:53:35 +0100

    Add migrations

---
+++ database/migrations/2022_08_07_184618_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->unsignedBigInteger('doctor_id');
+            $table->string('time');
+            $table->integer('status')->default(0);
+            $table->string('date');
+            $table->timestamps();
+
+            $table->foreign('doctor_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
